cf3ad514dbfecd8225771168c8542aaf0a661b69
Jefriwahyu <memberjefry1@gmail.com>
2022-03-07 19:36:05 +0700

    Tugas 2 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,18 @@
+var nomer = 21;
+console.log(nomer);
+
+var kata = "Bismillah";
+console.log(kata);
+
+var benar = true;
+var salah = false;
+console.log(benar);
+console.log(salah);
+
+var kosong = null;
+var hilang;
+console.log(kosong);
+console.log(hilang);
+
+var angka = [1, 25, 50, 75, 100];
+console.log(angka[4]);
